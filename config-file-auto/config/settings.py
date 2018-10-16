5ae49df8523d456b27698c4063cb3feb2b1ae746
huruizhi <huruizhi@pystandard.com>
2018-10-16 23:54:04 +0800

    自动创建jenkins配置 自动创建k8s配置文件

---
+++ config-file-auto/config/settings.py
@@ -0,0 +1,20 @@
+project_info = {
+    'harbor_url': 'harbor.pycf.com',
+    'project_name': 'pymom',
+    'stages': ['pro', 'dev'],
+    'register': ['register', 6553, 6503, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-register', 'java'],
+    'modules': [['home', 6550, 6500, 'svn://192.168.0.151/pycf/Projects/MOMsystem/pyMomSystem-home-1.0', 'java'],
+                ['managerData', 6551, 6501, 'svn://192.168.0.151/pycf/Projects/MOMsystem/pyMomSystem-managerData-1.0', 'java'],
+                ['managerSelect', 6552, 6502, 'svn://192.168.0.151/pycf/Projects/MOMsystem/pyMomSystem-managerSelect-1.0', 'java'],
+                ['outsourceZuulClient', 6554, 6504, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-zuulClient', 'java'],
+                ['manager', 6555, 6505, 'svn://192.168.0.151/pycf/Projects/MOMsystem/pyMomSystem-manager-1.0', 'java'],
+                ['outsourceHome', 6556,  6506, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-home', 'java'],
+                ['outsourcePosition', 6557, 6507, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-position', 'java'],
+                ['outsourceAnalysis', 6558, 6508, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-analysis', 'java'],
+                ['outsourceBenchmark', 6559, 6509, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-benchmark', 'java'],
+                ['outsourceGroup', 6560, 6510, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-group', 'java'],
+                ['outsourceCalculate', 6561, 6511, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-calculate', 'java'],
+                ['outsourceBackstageSystem', 6562, 6512, 'svn://192.168.0.151/pycf/Projects/MOMsystem/outsource-tycqls1.0-backstage-system', 'java'],
+                ['outsourceManagementSystem', 6563, 6513, 'svn://192.168.0.151/pycf/Projects/MOMsystem/OutsourcingManagementSystem', 'python']]
+}
+
